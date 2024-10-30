f93fb12091704ab008ff9fa8a9afe97a44a650c1
Matt Owen <matthew_owen@lusherschool.org>
2024-10-30 15:19:03 -0500

    Update DriveCode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode.java
@@ -37,47 +37,33 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-
 @TeleOp(name="DriveCode")
 //@Disabled
 @Config
 public class DriveCode extends LinearOpMode {
 
-    // Declare OpMode members.
-   // private DcMotor arm;
-   // private DcMotor intake;
-private DcMotor leftBack;
-   private DcMotor rightBack;
-   private DcMotor leftFront;
-   private DcMotor rightFront;
+    private DcMotor leftBack;
+    private DcMotor rightBack;
+    private DcMotor leftFront;
+    private DcMotor rightFront;
     private CRServo intakeServo;
     private Servo armServo;
     private DcMotor elevatorMotor;
-    //private Servo boxServo;
     private Servo slideServo;
     private Servo bucketServo;
 
-    //variables for arm and bucket height
-    //JakeTake Values //ed
-//    public static int scoringHeight = 235;
-//    public static int drivingHeight = 1411;
-//    public static int startingHeight = 0;
-//    public static int floorHeight = 1604;
-    public static double inspeed = -1;
-    public static double outspeed = 0.3;
+    public static double inSpeed = -1;
+    public static double outSpeed = 1;
     public static int startingHeight = 0;
-    public static int scoringHeight = 100;
-    public static int maxHeight = 0;
-    public static int pickupHeight = -100;
+    public static int highBucketHeight = -4000;
+    public static int lowBucketHeight = -2000;
     public static double inHeight = 0.03;
     public static double outHeight = 0.65;
     public static double midHeight = 0.25;
     public static double slidesOut = 0.3;
-    public static double slidesIn = 0.55;
+    public static double slidesIn = 0.4;
     public static double bucketIntake = 0;
     public static double bucketOut = 0;
-
-
 
     @Override
     public void runOpMode() {
@@ -87,14 +73,8 @@ private DcMotor leftBack;
        double slidePosition = 0.55;
        double bucketPosition = 0;
 
-
-
-       // arm = hardwareMap.get(DcMotor.class, "arm");
-      //  intake = hardwareMap.get(DcMotor.class, "Intake");
-        //control_Hub = hardwareMap.get(Blinker.class, "Control Hub");
-        //expansion_Hub_2 = hardwareMap.get(Blinker.class, "Expansion Hub 2");
         leftBack = hardwareMap.get(DcMotor.class, "leftBack");
-       rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
         intakeServo = hardwareMap.get(CRServo.class,"intakeServo");
@@ -102,24 +82,9 @@ private DcMotor leftBack;
         elevatorMotor = hardwareMap.get(DcMotor.class, "elevatorMotor");
         bucketServo = hardwareMap.get(Servo.class, "bucketServo");
         slideServo = hardwareMap.get(Servo.class, "slideServo");
-       // imu = hardwareMap.get(IMU.class, "imu");
 
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-
-        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
-        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
-        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-//        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
-//        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
         elevatorMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-
-        // Wait for the game to start (driver presses START)
         waitForStart();
         if (isStopRequested()) return;
 
@@ -128,9 +93,6 @@ private DcMotor leftBack;
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
 
-//             Denominator is the largest motor power (absolute value) or 1
-//             This ensures all the powers maintain the same ratio,
-//             but only if at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (y + x + rx) / denominator;
             double backLeftPower = (y - x + rx) / denominator;
@@ -143,48 +105,42 @@ private DcMotor leftBack;
             rightBack.setPower(backRightPower);
 
             if (gamepad1.a || gamepad2.a){
-                speed = inspeed;
-
+                speed = inSpeed;
             }
             if (gamepad1.b || gamepad2.b){
-
-                speed = outspeed;
+                speed = outSpeed;
             }
-            if (gamepad2.start) {
+            if (gamepad2.start || gamepad1.options) {
                speed = 0;
             }
             // Outake position
             if (gamepad2.y){
-                armHeight = (inHeight);
+                armHeight = inHeight;
             }
             //Intake Position
             if (gamepad2.x){
-                armHeight = (outHeight);
+                armHeight = outHeight;
             }
             // Middle position
             if (gamepad2.right_stick_button){
-                armHeight = (midHeight);
+                armHeight = midHeight;
             }
 
-            if (gamepad2.dpad_up){
+            if (gamepad2.dpad_down){
                 elevatorMotor.setTargetPosition(startingHeight);
                 elevatorMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                elevatorMotor.setPower (0.5);
+                elevatorMotor.setPower (1);
             }
-            if (gamepad2.dpad_down){
-                elevatorMotor.setTargetPosition(scoringHeight);
+            if (gamepad2.dpad_up){
+                elevatorMotor.setTargetPosition(highBucketHeight);
                 elevatorMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                elevatorMotor.setPower (0.5);
+                elevatorMotor.setPower (1);
             }
-            if (gamepad2.dpad_left){
-                elevatorMotor.setTargetPosition(maxHeight);
-                elevatorMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                elevatorMotor.setPower (0.5);
-            }
+
             if (gamepad2.dpad_right){
-                elevatorMotor.setTargetPosition(pickupHeight);
+                elevatorMotor.setTargetPosition(lowBucketHeight);
                 elevatorMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                elevatorMotor.setPower (0.5);
+                elevatorMotor.setPower (1);
             }
             if (gamepad2.left_bumper){
                 slidePosition = slidesIn;
@@ -206,30 +162,8 @@ private DcMotor leftBack;
           bucketServo.setPosition(bucketPosition);
 
 
-
-
-//            if (gamepad1.y) {
-//                arm.setTargetPosition(floorHeight);
-//                arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                arm.setPower (0.5);
-//            }
-//
-//            if (gamepad1.left_bumper) {
-//                arm.setTargetPosition(drivingHeight);
-//                arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                arm.setPower(0.5);
-//            }
-//
-//            else if (gamepad1.right_bumper){
-//                arm.setTargetPosition(startingHeight);
-//                arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                arm.setPower(0.5);
-//            }
-
-
             telemetry.addData("Status", "Running");
-           // telemetry.addData ("ArmHeight:", arm.getCurrentPosition());
-            telemetry.addData("speed",  speed);
+            telemetry.addData("lift height",  elevatorMotor.getCurrentPosition());
             telemetry.update();
 
         }
