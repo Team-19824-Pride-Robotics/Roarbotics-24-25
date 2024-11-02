f14bf371d8bb809001b26a67c6926c9218606718
IvanFortenberry <ivan_fortenberry.stu@willowschoolnola.org>
2024-11-02 09:59:51 -0500

    Updated horizontal slides value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCode.java
@@ -60,8 +60,8 @@ public class DriveCode extends LinearOpMode {
     public static double inHeight = 0.03;
     public static double outHeight = 0.65;
     public static double midHeight = 0.25;
-    public static double slidesOut = 0.3;
-    public static double slidesIn = 0.33;
+    public static double slidesOut = 0.85;
+    public static double slidesIn = 0.83;
     public static double bucketIntake = 0.55;
     public static double bucketOut = 0.02;
     public static double bucketMid = 0.3;
@@ -71,7 +71,7 @@ public class DriveCode extends LinearOpMode {
 
        double speed = 0;
        double armHeight = 0.65;
-       double slidePosition = 0;
+       double slidePosition = 1;
        double bucketPosition = 0.3;
 
         leftBack = hardwareMap.get(DcMotor.class, "leftBack");
